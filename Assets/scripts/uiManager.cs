2411d10b17986d61df78fbdc2723e2109e566c9d
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-06-06 09:01:24 -0300

    dinero UI sin terminar

---
+++ Assets/scripts/uiManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiManager : MonoBehaviour
+{
+    public DineroManager dinero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dinero.playerMoney
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
