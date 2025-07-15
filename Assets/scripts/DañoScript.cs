e366ad591e03e4c58c6698ef07e22108516b10af
Jeremiasbercellini <167231647+Jeremiasbercellini@users.noreply.github.com>
2025-07-15 11:35:34 -0300

    VIda terminada

--- Assets/scripts/DañoScript.cs
+++ Assets/scripts/DañoScript.cs
@@ -7,21 +7,11 @@ public class DañoScript : MonoBehaviour
     public float daño = -10f;
     public VidaManager vidaManager;
 
-    public void Start()
-    {
-        Debug.Log("existooo o!!11!1!");
-    }
-
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Player hit!!!!!");
             vidaManager.UpdateLife(daño);
-        }
-        else
-        {
-            Debug.Log("NOT Player!!11!1!");
         }
     }
 
